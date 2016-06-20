624b00be3196944c3610d5c205891994043e7174
Jae-hoon Park <fletpi@naver.com>
2016-06-20 09:23:57 +0900

    VO에 Getter/Setter 등 안 만들어진 걸 추가.

--- udongca_project/src/kr/co/udongca/vo/Address.java
+++ udongca_project/src/kr/co/udongca/vo/Address.java
@@ -1,8 +1,63 @@
 package kr.co.udongca.vo;
 
-public class Address {
+import java.io.Serializable;
 
+public class Address implements Serializable{
 	private String AddressId;
 	private String AddressName;
 	
+	public Address(){}
+	public Address(String addressId, String addressName) {
+		AddressId = addressId;
+		AddressName = addressName;
+	}
+	
+	public String getAddressId() {
+		return AddressId;
+	}
+	public String getAddressName() {
+		return AddressName;
+	}
+	public void setAddressId(String addressId) {
+		AddressId = addressId;
+	}
+	public void setAddressName(String addressName) {
+		AddressName = addressName;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((AddressId == null) ? 0 : AddressId.hashCode());
+		result = prime * result + ((AddressName == null) ? 0 : AddressName.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (AddressId == null) {
+			if (other.AddressId != null)
+				return false;
+		} else if (!AddressId.equals(other.AddressId))
+			return false;
+		if (AddressName == null) {
+			if (other.AddressName != null)
+				return false;
+		} else if (!AddressName.equals(other.AddressName))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Address [AddressId=" + AddressId + ", AddressName=" + AddressName + "]";
+	}
 }
--- udongca_project/src/kr/co/udongca/vo/ReportBoard.java
+++ udongca_project/src/kr/co/udongca/vo/ReportBoard.java
@@ -15,6 +15,182 @@ public class ReportBoard {
 	
 	public ReportBoard() {
 	}
+
+	public ReportBoard(int reportboardNo, String reportMemberId, String reportReason, String reportContent,
+			String reportResult, String reportCancelReason, String reportType, int reportNO, String memberId,
+			Member member) {
+		this.reportboardNo = reportboardNo;
+		this.reportMemberId = reportMemberId;
+		this.reportReason = reportReason;
+		this.reportContent = reportContent;
+		this.reportResult = reportResult;
+		this.reportCancelReason = reportCancelReason;
+		this.reportType = reportType;
+		this.reportNO = reportNO;
+		this.memberId = memberId;
+		this.member = member;
+	}
+
+	public int getReportboardNo() {
+		return reportboardNo;
+	}
+
+	public String getReportMemberId() {
+		return reportMemberId;
+	}
+
+	public String getReportReason() {
+		return reportReason;
+	}
+
+	public String getReportContent() {
+		return reportContent;
+	}
+
+	public String getReportResult() {
+		return reportResult;
+	}
+
+	public String getReportCancelReason() {
+		return reportCancelReason;
+	}
+
+	public String getReportType() {
+		return reportType;
+	}
+
+	public int getReportNO() {
+		return reportNO;
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public Member getMember() {
+		return member;
+	}
+
+	public void setReportboardNo(int reportboardNo) {
+		this.reportboardNo = reportboardNo;
+	}
+
+	public void setReportMemberId(String reportMemberId) {
+		this.reportMemberId = reportMemberId;
+	}
+
+	public void setReportReason(String reportReason) {
+		this.reportReason = reportReason;
+	}
+
+	public void setReportContent(String reportContent) {
+		this.reportContent = reportContent;
+	}
+
+	public void setReportResult(String reportResult) {
+		this.reportResult = reportResult;
+	}
+
+	public void setReportCancelReason(String reportCancelReason) {
+		this.reportCancelReason = reportCancelReason;
+	}
+
+	public void setReportType(String reportType) {
+		this.reportType = reportType;
+	}
+
+	public void setReportNO(int reportNO) {
+		this.reportNO = reportNO;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public void setMember(Member member) {
+		this.member = member;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((member == null) ? 0 : member.hashCode());
+		result = prime * result + ((memberId == null) ? 0 : memberId.hashCode());
+		result = prime * result + ((reportCancelReason == null) ? 0 : reportCancelReason.hashCode());
+		result = prime * result + ((reportContent == null) ? 0 : reportContent.hashCode());
+		result = prime * result + ((reportMemberId == null) ? 0 : reportMemberId.hashCode());
+		result = prime * result + reportNO;
+		result = prime * result + ((reportReason == null) ? 0 : reportReason.hashCode());
+		result = prime * result + ((reportResult == null) ? 0 : reportResult.hashCode());
+		result = prime * result + ((reportType == null) ? 0 : reportType.hashCode());
+		result = prime * result + reportboardNo;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ReportBoard other = (ReportBoard) obj;
+		if (member == null) {
+			if (other.member != null)
+				return false;
+		} else if (!member.equals(other.member))
+			return false;
+		if (memberId == null) {
+			if (other.memberId != null)
+				return false;
+		} else if (!memberId.equals(other.memberId))
+			return false;
+		if (reportCancelReason == null) {
+			if (other.reportCancelReason != null)
+				return false;
+		} else if (!reportCancelReason.equals(other.reportCancelReason))
+			return false;
+		if (reportContent == null) {
+			if (other.reportContent != null)
+				return false;
+		} else if (!reportContent.equals(other.reportContent))
+			return false;
+		if (reportMemberId == null) {
+			if (other.reportMemberId != null)
+				return false;
+		} else if (!reportMemberId.equals(other.reportMemberId))
+			return false;
+		if (reportNO != other.reportNO)
+			return false;
+		if (reportReason == null) {
+			if (other.reportReason != null)
+				return false;
+		} else if (!reportReason.equals(other.reportReason))
+			return false;
+		if (reportResult == null) {
+			if (other.reportResult != null)
+				return false;
+		} else if (!reportResult.equals(other.reportResult))
+			return false;
+		if (reportType == null) {
+			if (other.reportType != null)
+				return false;
+		} else if (!reportType.equals(other.reportType))
+			return false;
+		if (reportboardNo != other.reportboardNo)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "ReportBoard [reportboardNo=" + reportboardNo + ", reportMemberId=" + reportMemberId + ", reportReason="
+				+ reportReason + ", reportContent=" + reportContent + ", reportResult=" + reportResult
+				+ ", reportCancelReason=" + reportCancelReason + ", reportType=" + reportType + ", reportNO=" + reportNO
+				+ ", memberId=" + memberId + ", member=" + member + "]";
+	}
 	
 	
 }
