4ca3a9f9fdc42b2ad96ba555cdcb9c07353a0e14
Jae-hoon Park <fletpi@naver.com>
2016-06-21 17:29:02 +0900

    Mapper, DAO, Service 추가 및 수정.
    모의 Controller로 시험했을 때 발견한 오류들 수정.
    주소 검색에 관한 Mapper, DAO, Service 추가.

--- udongca_project/src/kr/co/udongca/dao/SearchDao.java
+++ udongca_project/src/kr/co/udongca/dao/SearchDao.java
@@ -2,9 +2,12 @@ package kr.co.udongca.dao;
 
 import java.util.List;
 
+import kr.co.udongca.vo.Address;
 import kr.co.udongca.vo.PRBoard;
 
 public interface SearchDao {
+	public List<Address> selectAllMajorAddress();
+	public List<Address> selectMiddleAddressByMajorAddressNo(int majorAddressNo);
 	public List<PRBoard> selectPRBoardListByAddress (String cafeAddress);
 	public List<PRBoard> selectPRBoardListByFeature (String cafeFeature);
 }
--- udongca_project/src/kr/co/udongca/service/impl/SearchServiceImpl.java
+++ udongca_project/src/kr/co/udongca/service/impl/SearchServiceImpl.java
@@ -7,12 +7,23 @@ import org.springframework.stereotype.Service;
 
 import kr.co.udongca.dao.SearchDao;
 import kr.co.udongca.service.SearchService;
+import kr.co.udongca.vo.Address;
 import kr.co.udongca.vo.PRBoard;
 
 @Service
 public class SearchServiceImpl implements SearchService{
 	@Autowired
 	private SearchDao dao;
+	
+	@Override
+	public List<Address> selectAllMajorAddress() {
+		return dao.selectAllMajorAddress();
+	}
+
+	@Override
+	public List<Address> selectMiddleAddressByMajorAddressNo(int majorAddressNo) {
+		return dao.selectMiddleAddressByMajorAddressNo(majorAddressNo);
+	}
 	
 	@Override
 	public List<PRBoard> selectPRBoardListByAddress(String cafeAddress) {
