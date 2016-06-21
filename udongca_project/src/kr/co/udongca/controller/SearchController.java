7103d2171151e1f5d83d1f9afa112f27e9090a19
kosta <kosta@192.168.0.111>
2016-06-21 18:23:42 +0900

    Annotation, Maven 추가
    SearchController에 @ResponseBody 추가
    Maven에 Jackson-Databind 추가

--- udongca_project/src/kr/co/udongca/controller/SearchController.java
+++ udongca_project/src/kr/co/udongca/controller/SearchController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.co.udongca.service.SearchService;
+import kr.co.udongca.vo.Address;
 import kr.co.udongca.vo.PRBoard;
 
 @Controller
@@ -15,12 +17,28 @@ public class SearchController {
 	@Autowired
 	private SearchService service;
 	
+	@RequestMapping("selectAllMajorAddress.udc")
+	@ResponseBody
+	public List<Address> selectAllMajorAddress(){
+		System.out.println("selectAllMajorAddress");
+		System.out.println(service.selectAllMajorAddress());
+		return service.selectAllMajorAddress();
+	}
+	
+	@RequestMapping("selectMiddleAddressByMajorAddressNo.udc")
+	@ResponseBody
+	public List<Address> selectMiddleAddressByMajorAddressNo(int majorAddressNo){
+		return service.selectMiddleAddressByMajorAddressNo(majorAddressNo);
+	}
+	
 	@RequestMapping("locationSearchResult.udc")
+	@ResponseBody
 	public List<PRBoard> locationSearchResult(String cafeAddress){
 		return service.selectPRBoardListByAddress(cafeAddress);
 	}
 	
 	@RequestMapping("themeSearchResult.udc")
+	@ResponseBody
 	public List<PRBoard> themeSearchResult(String cafeFeature){
 		return service.selectPRBoardListByFeature(cafeFeature);
 	}
